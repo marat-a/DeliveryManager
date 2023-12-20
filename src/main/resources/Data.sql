ad00cd5c52512ad1539df3e8b7071e85acc25433
marat-a <detishki66@yandex.ru>
2023-12-21 00:33:29 +0500

    refactoring models

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -3,3 +3,9 @@ INSERT INTO roles (name) VALUES ('ADMIN');
 INSERT INTO roles (name) VALUES ('COURIER');
 INSERT INTO roles (name) VALUES ('MANAGER');
 INSERT INTO roles (name) VALUES ('CUSTOMER');
+
+INSERT INTO users (login,name,password,phone,id) VALUES ('admin','Marat','$2a$10$lrHxwoGiY2Z2/tTca.4FUeiMZLQnX3McmDduD8jG2FOFniQ22WLru','89122341594','1');
+INSERT INTO user_roles (user_id,role_id) VALUES (1,1);
+INSERT INTO user_roles (user_id,role_id) VALUES (1,2);
+INSERT INTO user_roles (user_id,role_id) VALUES (1,3);
+INSERT INTO user_roles (user_id,role_id) VALUES (1,4);
